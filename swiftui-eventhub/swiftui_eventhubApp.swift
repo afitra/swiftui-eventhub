b332d953556c26d292bb9205600334eb1454e7c9
afitra <loeby45@gmail.com>
2023-04-16 20:48:16 +0700

    Initial Commit

---
+++ swiftui-eventhub/swiftui_eventhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_eventhubApp.swift
+//  swiftui-eventhub
+//
+//  Created by afitra mamor on 16/04/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_eventhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
